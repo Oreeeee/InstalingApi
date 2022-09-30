5766d8ef0ae0b39f7931f8a3516b0c910301970f
Oreeeee <lifehackeeer2@gmail.com>
2022-09-30 23:04:09 +0200

    fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="instalingapi",
-    version="1.0",
+    version="1.1-fix01",
     description="Instaling API written in Python.",
     license="Unlicense",
     author="Oreeeee",
