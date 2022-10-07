1f74510163c1e86350629345baa19ee9631a88fb
Oreeeee <lifehackeeer2@gmail.com>
2022-10-08 01:39:11 +0200

    bump ver number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="instalingapi",
-    version="1.2.3",
+    version="1.2.5-test2",
     description="Instaling API written in Python.",
     license="Unlicense",
     author="Oreeeee",
