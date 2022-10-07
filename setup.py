a731660f881089bd0d7608989bf30272c6690868
Oreeeee <lifehackeeer2@gmail.com>
2022-10-08 01:26:41 +0200

    fdfd

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="instalingapi",
-    version="1.2.4",
+    version="1.2.5.test1",
     description="Instaling API written in Python.",
     license="Unlicense",
     author="Oreeeee",
