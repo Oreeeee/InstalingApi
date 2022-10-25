2d82715edad069b904c484ce7c4c30a3aee9c13d
Oreeeee <lifehackeeer2@gmail.com>
2022-10-25 20:20:39 +0200

    change string splice

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="instalingapi",
-    version="1.2.5",
+    version="1.2.5-dev1",
     description="Instaling API written in Python.",
     license="Unlicense",
     author="Oreeeee",
