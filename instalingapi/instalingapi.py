9cffe774526439fa6b34748f3b789360d5db9ce8
Oreeeee <lifehackeeer2@gmail.com>
2022-10-25 20:28:07 +0200

    update

--- instalingapi/instalingapi.py
+++ instalingapi/instalingapi.py
@@ -27,7 +27,7 @@ class InstalingAPI:
         try:
             # Get user ID
             soup = BeautifulSoup(self.student_page.text)
-            id_button = soup.find("a", class_="btn_session")
+            id_button = soup.find_all("a", class_="btn_session")
             self.instaling_id = id_button.get("href")[-8:-1]
         except IndexError:
             raise WrongPasswordException
